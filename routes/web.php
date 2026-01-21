82d623aef62b0178e19354b8986e26a58e572eda
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2026-01-21 17:00:45 +0600

    clean project.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserSelectionController;
 use App\Http\Controllers\AvailabilityController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,7 +13,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [AvailabilityController::class, 'index'])->name('dashboard');
     Route::get('/availability', [AvailabilityController::class, 'index'])->name('availability.index');
     Route::post('/availability', [AvailabilityController::class, 'store'])->name('availability.store');
+
+    Route::controller(UserSelectionController::class)->middleware(['admin'])->prefix('admin')->name('admin.')->group([], function () {
+        Route::get('/users/list', 'getUsers')->name('users.list');
+        Route::get('/users/{userId}/availability', 'getUserAvailability')->name('users.availability');
+    });
 });
 
 require __DIR__ . '/settings.php';
-require __DIR__ . '/admin.php';
