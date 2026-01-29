19fc3e0198447e7c93aebf8757e8c482c74c5f26
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2026-01-29 14:04:42 +0600

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,13 +13,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PROTO);
-
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
